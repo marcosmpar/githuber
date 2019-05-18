194413a1bbe42a5e08fbf7273543dfa856e41f7f
Marcos Rodrigues <marcosmpar@gmail.com>
2019-05-18 15:33:23 -0400

    Implementações de rotas e criação do Header

--- src/pages/Repositores/index.js
+++ src/pages/Repositores/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 import { View } from 'react-native';
 
+import Header from '../../components/Header';
+
 // import { Container } from './styles';
 
-const Repositores = () => <View />;
+const Repositores = () => (
+  <View>
+    <Header title="Repositórios" />
+  </View>
+);
 
 export default Repositores;
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,16 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import Welcome from './pages/Welcome';
 import Repositores from './pages/Repositores';
 
-const Routes = createAppContainer(
-  createSwitchNavigator({
-    Welcome,
-    Repositores,
-  }),
+const Routes = (userLogged = false) => createAppContainer(
+  createSwitchNavigator(
+    {
+      Welcome,
+      Repositores,
+    },
+    {
+      initialRouteName: userLogged ? 'Repositores' : 'Welcome',
+    },
+  ),
 );
 
 export default Routes;
