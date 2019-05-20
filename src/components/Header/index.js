3f35cd8cba5fce4dfa77b13f9fc27390204d6d18
Marcos Rodrigues <marcosmpar@gmail.com>
2019-05-20 19:29:06 -0400

    fix Header/Repositories

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,45 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import AsyncStorage from '@react-native-community/async-storage';
-import { withNavigation } from 'react-navigation';
 
-=======
 import { withNavigation } from 'react-navigation';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import AsyncStorage from '@react-native-community/async-storage';
->>>>>>> 67eae0c60c413c2f1b27746e39b40bda744ab02a
-=======
-import { withNavigation } from 'react-navigation';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import AsyncStorage from '@react-native-community/async-storage';
->>>>>>> 67eae0c60c413c2f1b27746e39b40bda744ab02a
 import {
   View, Text, TouchableOpacity, StatusBar,
 } from 'react-native';
-
-import Icon from 'react-native-vector-icons/FontAwesome';
 
 import styles from './styles';
 
 class Header extends Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
-<<<<<<< HEAD
-<<<<<<< HEAD
-  };
-
-  signOut = async () => {
-    await AsyncStorage.clear();
-  };
-
-  render() {
-    const { title } = this.props;
-
-=======
-=======
->>>>>>> 67eae0c60c413c2f1b27746e39b40bda744ab02a
     navigation: PropTypes.shape({
       navigate: PropTypes.func,
     }).isRequired,
@@ -55,10 +28,6 @@ class Header extends Component {
 
   render() {
     const { title } = this.props;
-<<<<<<< HEAD
->>>>>>> 67eae0c60c413c2f1b27746e39b40bda744ab02a
-=======
->>>>>>> 67eae0c60c413c2f1b27746e39b40bda744ab02a
     return (
       <View style={styles.container}>
         <StatusBar barStyle="dark-content" />
