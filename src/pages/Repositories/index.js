e447b3e2425bbe1b94760e5aaf2425ca822a2368
Marcos Rodrigues <marcosmpar@gmail.com>
2019-05-20 21:57:15 -0400

    Repos API

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
-import { View } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 
 import PropTypes from 'prop-types';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
+import AsyncStorage from '@react-native-community/async-storage';
+import api from '../../services/api';
 
 import Header from '../../components/Header';
 
-// import { Container } from './styles';
+import styles from './styles';
 
 const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
 
@@ -23,12 +25,24 @@ export default class Repositories extends Component {
 
   state = {
     data: [],
+    loading: true,
   };
 
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const { data } = await api.get(`/users/${username}/repos`);
+
+    this.setState({ data, loading: false });
+  }
+
+  renderList = () => <Text>Lista</Text>;
+
   render() {
+    const { loading } = this.state;
     return (
       <View>
         <Header title="Repositórios" />
+        {loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
       </View>
     );
   }
